a95b5ffa041fbc8e9945041e14f1a63d6896253e
Mehmet Uzel <98mehmetuzel@gmail.com>
2021-12-05 14:56:23 +0300

    Custom Forms

--- ecommsite/views.py
+++ ecommsite/views.py
@@ -5,9 +5,27 @@ from .models import ToDoList,Item
 from .forms import CreateNewList
 
 def index(response, id):
-    #ls=ToDoList.objects.get(name=name)
     ls = ToDoList.objects.get(id=id)
-    #return HttpResponse("<h1> %s </h1><br><p>%s</p>" %(ls.name, item.text))
+
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c"+ str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_set.create(text = txt, complete = False)
+            else:
+                print("invalid")
+
+
     return render(response, "ecommsite/list.html", {"ls":ls})
 
 def home(response):
@@ -21,7 +39,6 @@ def create(response):
             n = form.cleaned_data["name"]
             t = ToDoList(name = n)
             t.save()
-        print(t.id)
         return HttpResponseRedirect("/%i" %t.id)
 
     else:
