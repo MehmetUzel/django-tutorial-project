518eb0bb747a106aab8d7ae6da98c798b1ca2d24
Mehmet Uzel <98mehmetuzel@gmail.com>
2021-12-07 18:22:44 +0300

    User Specific Pages and Access :apple:

--- ecommsite/views.py
+++ ecommsite/views.py
@@ -7,26 +7,28 @@ from .forms import CreateNewList
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
 
-    if response.method == "POST":
-        print(response.POST)
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c"+ str(item.id)) == "clicked":
-                    item.complete = True
+    if ls in response.user.todolist.all():
+
+        if response.method == "POST":
+            print(response.POST)
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c"+ str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    item.save()
+
+            elif response.POST.get("newItem"):
+                txt = response.POST.get("new")
+
+                if len(txt) > 2:
+                    ls.item_set.create(text = txt, complete = False)
                 else:
-                    item.complete = False
-                item.save()
+                    print("invalid")
+        return render(response, "ecommsite/list.html", {"ls":ls})
 
-        elif response.POST.get("newItem"):
-            txt = response.POST.get("new")
-
-            if len(txt) > 2:
-                ls.item_set.create(text = txt, complete = False)
-            else:
-                print("invalid")
-
-
-    return render(response, "ecommsite/list.html", {"ls":ls})
+    return render(response, "ecommsite/view.html", {})
 
 def home(response):
     return render(response, "ecommsite/home.html", {})
@@ -38,11 +40,16 @@ def create(response):
 
         if form.is_valid():
             n = form.cleaned_data["name"]
-            t = ToDoList(name = n)
-            t.save()
+            t = ToDoList(name=n)
+            t.save
+            response.user.todolist.add(t)
+
         return HttpResponseRedirect("/%i" %t.id)
 
     else:
         form = CreateNewList()
     return render(response, "ecommsite/create.html", {"form":form})
 
+
+def view(response):
+    return render(response, "ecommsite/view.html", {})
