d2031bb8b8be6d381c8e0883367ecce99937312b
Mehmet Uzel <98mehmetuzel@gmail.com>
2021-12-07 15:34:25 +0300

    User Registration and Signup Page :8ball:

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'ecommsite.apps.EcommsiteConfig',
+    'register.apps.RegisterConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
